0974061ec6141d0db9ad22e79a63d099c3f954ff
Kristoffer Nordstroem <kristoffer.nordstroem@scs.ch>
2022-12-05 13:49:16 +0100

    clang-format -style=LLVM -i *.c *.h

--- recipes-dmatest/altera-fpga-pcie-dma/files/user.c
+++ recipes-dmatest/altera-fpga-pcie-dma/files/user.c
@@ -150,127 +150,128 @@ int main() {
     read_line(line, BUFFER_LENGTH, stdin);
     num_input = strtol(line, NULL, 10);
     switch (num_input) {
-      case ALTERA_EXIT:
+    case ALTERA_EXIT:
+      break;
+    case ALTERA_CMD_START_DMA:
+      ioctl(f, ALTERA_IOCX_START);
+      ioctl(f, ALTERA_IOCX_WAIT);
+      cmd.cmd = ALTERA_CMD_READ_STATUS;
+      cmd.buf = buf;
+      write(f, &cmd, 0);
+      break;
+    case ALTERA_CMD_ENA_DIS_READ:
+      cmd.cmd = ALTERA_CMD_ENA_DIS_READ;
+      cmd.buf = buf;
+      write(f, &cmd, 0);
+      break;
+    case ALTERA_CMD_ENA_DIS_WRITE:
+      cmd.cmd = ALTERA_CMD_ENA_DIS_WRITE;
+      cmd.buf = buf;
+      write(f, &cmd, 0);
+      break;
+    case ALTERA_CMD_ENA_DIS_SIMUL:
+      cmd.cmd = ALTERA_CMD_ENA_DIS_SIMUL;
+      cmd.buf = buf;
+      write(f, &cmd, 0);
+      break;
+    case ALTERA_CMD_MODIFY_NUM_DWORDS:
+      cmd.cmd = ALTERA_CMD_MODIFY_NUM_DWORDS;
+      cmd.buf = buf;
+      printf("enter # dwords: ");
+      read_line(line, BUFFER_LENGTH, stdin);
+      num_input = strtol(line, NULL, 10);
+      *(int *)buf = num_input;
+      if (num_input < 1 || num_input > 4096) {
+        // if (num_input < 1 || num_input > 0x3FFFF){
+        printf("the maximum transfer size of each descriptor is 0x3FFFF DW "
+               "(1MB)\n");
         break;
-      case ALTERA_CMD_START_DMA:
-        ioctl(f, ALTERA_IOCX_START);
-        ioctl(f, ALTERA_IOCX_WAIT);
-        cmd.cmd = ALTERA_CMD_READ_STATUS;
-        cmd.buf = buf;
+      } else
         write(f, &cmd, 0);
+      break;
+    case ALTERA_CMD_MODIFY_NUM_DESC:
+      cmd.cmd = ALTERA_CMD_MODIFY_NUM_DESC;
+      cmd.buf = buf;
+      printf("enter desc num: ");
+      read_line(line, BUFFER_LENGTH, stdin);
+      num_input = strtol(line, NULL, 10);
+      *(int *)buf = num_input;
+      if (num_input > 128 || num_input < 1)
         break;
-      case ALTERA_CMD_ENA_DIS_READ:
-        cmd.cmd = ALTERA_CMD_ENA_DIS_READ;
-        cmd.buf = buf;
+      else
         write(f, &cmd, 0);
-        break;
-      case ALTERA_CMD_ENA_DIS_WRITE:
-        cmd.cmd = ALTERA_CMD_ENA_DIS_WRITE;
-        cmd.buf = buf;
-        write(f, &cmd, 0);
-        break;
-      case ALTERA_CMD_ENA_DIS_SIMUL:
-        cmd.cmd = ALTERA_CMD_ENA_DIS_SIMUL;
-        cmd.buf = buf;
-        write(f, &cmd, 0);
-        break;
-      case ALTERA_CMD_MODIFY_NUM_DWORDS:
-        cmd.cmd = ALTERA_CMD_MODIFY_NUM_DWORDS;
-        cmd.buf = buf;
-        printf("enter # dwords: ");
-        read_line(line, BUFFER_LENGTH, stdin);
-        num_input = strtol(line, NULL, 10);
-        *(int *)buf = num_input;
-        if (num_input < 1 || num_input > 4096) {
-          // if (num_input < 1 || num_input > 0x3FFFF){
-          printf(
-              "the maximum transfer size of each descriptor is 0x3FFFF DW "
-              "(1MB)\n");
-          break;
-        } else
+      break;
+    case ALTERA_CMD_ONCHIP_OFFCHIP:
+      cmd.cmd = ALTERA_CMD_ONCHIP_OFFCHIP;
+      cmd.buf = buf;
+      write(f, &cmd, 0);
+      break;
+    case ALTERA_CMD_RAND:
+      cmd.cmd = ALTERA_CMD_RAND;
+      cmd.buf = buf;
+      write(f, &cmd, 0);
+      break;
+    case ALTERA_LOOP:
+      printf(
+          "enter loop num (0 for infinite and press ESC to quit the loop): ");
+      read_line(line, BUFFER_LENGTH, stdin);
+      loop_num = strtol(line, NULL, 10);
+      if (loop_num != 0) {
+        for (i = 0; i < loop_num; i++) {
+          ioctl(f, ALTERA_IOCX_START);
+          ioctl(f, ALTERA_IOCX_WAIT);
+          cmd.cmd = ALTERA_CMD_READ_STATUS;
+          cmd.buf = buf;
           write(f, &cmd, 0);
-        break;
-      case ALTERA_CMD_MODIFY_NUM_DESC:
-        cmd.cmd = ALTERA_CMD_MODIFY_NUM_DESC;
-        cmd.buf = buf;
-        printf("enter desc num: ");
-        read_line(line, BUFFER_LENGTH, stdin);
-        num_input = strtol(line, NULL, 10);
-        *(int *)buf = num_input;
-        if (num_input > 128 || num_input < 1)
-          break;
-        else
-          write(f, &cmd, 0);
-        break;
-      case ALTERA_CMD_ONCHIP_OFFCHIP:
-        cmd.cmd = ALTERA_CMD_ONCHIP_OFFCHIP;
-        cmd.buf = buf;
-        write(f, &cmd, 0);
-        break;
-      case ALTERA_CMD_RAND:
-        cmd.cmd = ALTERA_CMD_RAND;
-        cmd.buf = buf;
-        write(f, &cmd, 0);
-        break;
-      case ALTERA_LOOP:
-        printf(
-            "enter loop num (0 for infinite and press ESC to quit the loop): ");
-        read_line(line, BUFFER_LENGTH, stdin);
-        loop_num = strtol(line, NULL, 10);
-        if (loop_num != 0) {
-          for (i = 0; i < loop_num; i++) {
-            ioctl(f, ALTERA_IOCX_START);
-            ioctl(f, ALTERA_IOCX_WAIT);
-            cmd.cmd = ALTERA_CMD_READ_STATUS;
-            cmd.buf = buf;
-            write(f, &cmd, 0);
-            if ((!((struct dma_status *)buf)->pass_read &&
-                 ((struct dma_status *)buf)->run_read) ||
-                (!((struct dma_status *)buf)->pass_write &&
-                 ((struct dma_status *)buf)->run_write) ||
-                (!((struct dma_status *)buf)->pass_simul &&
-                 ((struct dma_status *)buf)->run_simul)) {
-              system("clear");
-              print_menu(buf);
-              printf("DMA data error!\n");
-              printf("Type in dmesg to show more details!\n");
-              return;
-            }
+          if ((!((struct dma_status *)buf)->pass_read &&
+               ((struct dma_status *)buf)->run_read) ||
+              (!((struct dma_status *)buf)->pass_write &&
+               ((struct dma_status *)buf)->run_write) ||
+              (!((struct dma_status *)buf)->pass_simul &&
+               ((struct dma_status *)buf)->run_simul)) {
             system("clear");
             print_menu(buf);
-            printf("Press ESC to stop\n");
-            //						usleep(1000*250);
-            if (kbhit()) break;
+            printf("DMA data error!\n");
+            printf("Type in dmesg to show more details!\n");
+            return;
           }
-        } else {
-          do {
-            ioctl(f, ALTERA_IOCX_START);
-            ioctl(f, ALTERA_IOCX_WAIT);
-            cmd.cmd = ALTERA_CMD_READ_STATUS;
-            cmd.buf = buf;
-            write(f, &cmd, 0);
-            if ((!((struct dma_status *)buf)->pass_read &&
-                 ((struct dma_status *)buf)->run_read) ||
-                (!((struct dma_status *)buf)->pass_write &&
-                 ((struct dma_status *)buf)->run_write) ||
-                (!((struct dma_status *)buf)->pass_simul &&
-                 ((struct dma_status *)buf)->run_simul)) {
-              system("clear");
-              print_menu(buf);
-              printf("DMA data error!\n");
-              printf("Type in dmesg to show more details!\n");
-              return;
-            }
+          system("clear");
+          print_menu(buf);
+          printf("Press ESC to stop\n");
+          //						usleep(1000*250);
+          if (kbhit())
+            break;
+        }
+      } else {
+        do {
+          ioctl(f, ALTERA_IOCX_START);
+          ioctl(f, ALTERA_IOCX_WAIT);
+          cmd.cmd = ALTERA_CMD_READ_STATUS;
+          cmd.buf = buf;
+          write(f, &cmd, 0);
+          if ((!((struct dma_status *)buf)->pass_read &&
+               ((struct dma_status *)buf)->run_read) ||
+              (!((struct dma_status *)buf)->pass_write &&
+               ((struct dma_status *)buf)->run_write) ||
+              (!((struct dma_status *)buf)->pass_simul &&
+               ((struct dma_status *)buf)->run_simul)) {
             system("clear");
             print_menu(buf);
-            printf("Press ESC to stop\n");
-            //						usleep(1000*250);
-            if (kbhit()) break;
-          } while (1);
-        }
-        break;
-      default:
-        printf("%d is an invalid command\n", num_input);
+            printf("DMA data error!\n");
+            printf("Type in dmesg to show more details!\n");
+            return;
+          }
+          system("clear");
+          print_menu(buf);
+          printf("Press ESC to stop\n");
+          //						usleep(1000*250);
+          if (kbhit())
+            break;
+        } while (1);
+      }
+      break;
+    default:
+      printf("%d is an invalid command\n", num_input);
     }
     system("clear");
   } while (num_input != ALTERA_EXIT);
